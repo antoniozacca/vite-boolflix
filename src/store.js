9dd86d205578b54bcb96942c7045a515f982acd5
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-06 12:18:57 +0200

    fix api

--- src/store.js
+++ src/store.js
@@ -4,6 +4,6 @@ export const store = reactive ({
     results: [],
     info: {},
     apiUrlMovie: 'https://api.themoviedb.org/3/search/movie',
-    apiUrlTv: 'https://api.themoviedb.org/3/discover/tv',
+    apiUrlTv: 'https://api.themoviedb.org/3/search/tv',
     apiKey: '?api_key=6ce7dae274d1b0b6bf782a5f37d51b8e'
 });
